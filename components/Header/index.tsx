2e9b981e72225366048cb6ced2f2aa906fb558fa
Nazar Revutskyy <nrevutskyy@gmail.com>
2021-04-22 13:33:28 +0300

    Hide search & show about page

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
-import {Input, Button, message, Row, Col, Drawer, Typography, Menu, Dropdown} from 'antd';
+import {Button, message, Row, Col, Drawer, Menu, Dropdown} from 'antd';
 import {setLoginURL, userLogInAction} from '../../lib/actions';
 import {UserState} from '../../lib/reducers/user.reducer';
 import {productionMode} from '../../utilities/constants';
@@ -15,7 +15,6 @@ import LoginViaAM from "../LoginViaAM";
 import {LOGOUT} from "../../graphql/mutations";
 import { MenuOutlined, DownOutlined, LogoutOutlined, UserOutlined, BookOutlined } from '@ant-design/icons';
 
-const {Search} = Input;
 
 
 type Props = {
@@ -27,9 +26,6 @@ type Props = {
 const HeaderMenuContainer: React.FunctionComponent<Props> = ({user, userLogInAction, setLoginURL}) => {
   const router = useRouter();
   const {data: authMachineData} = useQuery(GET_AM_LOGIN_URL);
-
-  const onSearch = () => {
-  }
 
   const menu = (
     <Menu style={{minWidth: 150}}>
@@ -144,11 +140,11 @@ const HeaderMenuContainer: React.FunctionComponent<Props> = ({user, userLogInAct
           onClose={onClose}
           visible={visible}
         >
-          <Search
-            placeholder="Search for open source product or initiative"
-            onSearch={onSearch}
-          />
-          <Menu style={{borderRight: 0, marginTop: 10}}>
+          {/*<Search*/}
+          {/*  placeholder="Search for open source product or initiative"*/}
+          {/*  onSearch={onSearch}*/}
+          {/*/>*/}
+          <Menu style={{borderRight: 0}}>
             <Menu.Item key="0">
               <Link href="/">
                 <a className="text-grey-9">Work on Open Products</a>
@@ -161,7 +157,7 @@ const HeaderMenuContainer: React.FunctionComponent<Props> = ({user, userLogInAct
             </Menu.Item>
             <Menu.Item key="2">
               <Link href="/">
-                <a className="text-grey-9">Find Talent</a>
+                <a className="text-grey-9">About</a>
               </Link>
             </Menu.Item>
             {user?.claimedTask ?
@@ -198,61 +194,65 @@ const HeaderMenuContainer: React.FunctionComponent<Props> = ({user, userLogInAct
       </Row>
 
 
-      <Row align="middle" style={{height: 56, borderBottom: '1px solid #d9d9d9'}} className="header-desktop">
-        <Col span={10}>
-          <Row justify="center">
-            <Col style={{marginRight: 20}}>
-              <Typography.Link className="gray-link" href="/">Work on Open Products</Typography.Link>
+      <Row align="middle" justify="center" style={{height: 56, borderBottom: '1px solid #d9d9d9'}} className="header-desktop">
+        <Col xl={20} lg={22}>
+          <Row>
+            <Col span={10}>
+              <Row justify="start" align="middle">
+                <Col style={{marginRight: 20}}>
+                  <Link className="gray-link" href="/">Work on Open Products</Link>
+                </Col>
+                <Col style={{marginRight: 20}}>
+                  <Link className="gray-link" href="/product/add">Add Product</Link>
+                </Col>
+                <Col style={{marginRight: 20}}>
+                  <Link className="gray-link" href="/about">About</Link>
+                </Col>
+              </Row>
             </Col>
-            <Col style={{marginRight: 20}}>
-              <Typography.Link className="gray-link" href="/product/add">Add Product</Typography.Link>
-            </Col>
-            <Col style={{marginRight: 20}}>
-              <Typography.Link className="gray-link" href="">Find Talent</Typography.Link>
-            </Col>
-          </Row>
-        </Col>
 
-        <Col span={4}>
-          <Row justify="center">
-            <Link href="/">
-              <img src={Logo} alt="logo"/>
-            </Link>
-          </Row>
-        </Col>
-
-        <Col span={10}>
-          <Row align="middle" justify="center">
-            <Col style={{marginRight: 10}}>
-              <Search
-                placeholder="Search for open source product or initiative"
-                onSearch={onSearch}
-              />
+            <Col span={4}>
+              <Row justify="center">
+                <Link href="/">
+                  <img src={Logo} alt="logo"/>
+                </Link>
+              </Row>
             </Col>
-            <Col>
-              {
-                user && user.isLoggedIn ? (
-                  <Dropdown overlay={menu} placement="bottomRight" className="ml-15">
-                    <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
-                      <strong className="text-grey-9">{user.username}</strong>
-                      <DownOutlined className="text-grey-9 ml-5" />
-                    </a>
-                  </Dropdown>
-                ) : (
-                  <>{
-                    productionMode
-                      ? <LoginViaAM />
-                      : (
-                        <Button
-                          className="signIn-btn"
-                          onClick={() => router.push("/switch-test-user")}
-                        >
-                          Sign in
-                        </Button>
-                      )
-                  }</>
-                )
-              }
+
+            <Col span={10}>
+              <Row align="middle" justify="end">
+                {/*<Col style={{marginRight: 10}}>*/}
+                {/*  <Search*/}
+                {/*    placeholder="Search for open source product or initiative"*/}
+                {/*    onSearch={onSearch}*/}
+                {/*  />*/}
+                {/*</Col>*/}
+                <Col>
+                  {
+                    user && user.isLoggedIn ? (
+                      <Dropdown overlay={menu} placement="bottomRight" className="ml-15">
+                        <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+                          <strong className="text-grey-9">{user.username}</strong>
+                          <DownOutlined className="text-grey-9 ml-5" />
+                        </a>
+                      </Dropdown>
+                    ) : (
+                      <>{
+                        productionMode
+                          ? <LoginViaAM />
+                          : (
+                            <Button
+                              className="signIn-btn"
+                              onClick={() => router.push("/switch-test-user")}
+                            >
+                              Sign in
+                            </Button>
+                          )
+                      }</>
+                    )
+                  }
+                </Col>
+              </Row>
             </Col>
           </Row>
         </Col>
