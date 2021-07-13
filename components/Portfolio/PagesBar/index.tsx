db24d4204685519441a448d3328412431b7acbbf
Nazar <nrevutskyy@gmail.com>
2021-07-13 10:16:47 +0300

    update for portfolio page

--- components/Portfolio/PagesBar/index.tsx
+++ components/Portfolio/PagesBar/index.tsx
@@ -5,17 +5,20 @@ import {Button} from "antd";
 import {LeftOutlined, RightOutlined} from "@ant-design/icons";
 
 const PagesBar = ({number, active, changePage, hasNext, hasPrev}: PagesBarProps) => {
-    return (
-        <div style={{display: "flex", flexDirection: "row"}}>
-            <Button style={{margin: "36px 8px"}} disabled={!hasPrev} icon={<LeftOutlined/>}
-                    onClick={() => changePage(active - 1)}/>
-            {Array.from({length: number}, (p, i) => i + 1).map(pageNumber => (
-                <PageButton changePage={changePage} key={pageNumber} number={pageNumber}
-                            active={pageNumber === active}/>
-            ))}
-            <Button style={{margin: "36px 8px"}} disabled={!hasNext} icon={<RightOutlined/>}
-                    onClick={() => changePage(active + 1)}/>
-        </div>
+    return (<>
+            {number > 1 ? (
+                <div style={{display: "flex", flexDirection: "row"}}>
+                    <Button style={{margin: "36px 8px"}} disabled={!hasPrev} icon={<LeftOutlined/>}
+                            onClick={() => changePage(active - 1)}/>
+                    {Array.from({length: number}, (p, i) => i + 1).map(pageNumber => (
+                        <PageButton changePage={changePage} key={pageNumber} number={pageNumber}
+                                    active={pageNumber === active}/>
+                    ))}
+                    <Button style={{margin: "36px 8px"}} disabled={!hasNext} icon={<RightOutlined/>}
+                            onClick={() => changePage(active + 1)}/>
+                </div>
+            ) : null}
+        </>
     );
 }
 
