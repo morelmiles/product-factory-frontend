ed972eb74d6efdeb1137b1dae911def1a6dca42f
Ashik Zinnat Khan <ashrik@gmail.com>
2021-10-14 23:10:05 +0600

    feat: added fake login and development mode api endpoint

--- utilities/constants.ts
+++ utilities/constants.ts
@@ -24,6 +24,7 @@ export type TreeNode = {
 }
 
 // export const apiDomain = process.env.NODE_ENV === "development" ? "" : "https://open-united-backend.herokuapp.com";
-export const apiDomain = "";
+// export const apiDomain = "";
+export const apiDomain = process.env.APPLICATION_MODE === "development" ? "http://localhost:8000" : "";
 export const RICH_TEXT_EDITOR_WIDTH = 1000;
 export const productionMode = process.env.APPLICATION_MODE === "production";
